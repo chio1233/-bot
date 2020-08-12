7bd13dd141903cc915c3b77748f6873a731b08f2
chio1233 <69512569+chio1233@users.noreply.github.com>
2020-08-12 09:20:19 +0900

    Update dcb.py

--- dcb.py
+++ dcb.py
@@ -1,12 +1,17 @@
 import discord
 import os
 
+
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
+    print("login")
+    print(client.user.name)
     print(client.user.id)
-    print("ready")
+    print("------------------")
+    await client.change_presence(game=discord.Game(name='',type=1))
 
 @client.event
 async def on_message(message):
