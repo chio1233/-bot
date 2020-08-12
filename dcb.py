2caf64ccfa7d1c17eddb40a36caabfa3b95bf741
chio1233 <69512569+chio1233@users.noreply.github.com>
2020-08-12 13:59:10 +0900

    Update dcb.py

--- dcb.py
+++ dcb.py
@@ -28,10 +28,8 @@ async def on_message(message):
     if message.content.startswith("!레드"):
         await message.channel.send("레드 왕자님~~")        
     if message.content.startswith("!블루"):
-        await message.channel.send("레드친구 블루예여!")   
-        
-    
-    if message.content.startswith('!경고') :
+        await message.channel.send("레드친구 블루예여!")
+    if message.content.startswith('경고 부여') :
         author = message.guild.get_member(int(message.content[9:27]))
         file = openpyxl.load_workbook('경고.xlsx')
         sheet = file.active
@@ -43,9 +41,9 @@ async def on_message(message):
                 file.save("경고.xlsx")
                 if sheet["B" + str(i)].value == 2:
                     await message.guild.ban(author)
-                    await message.channel.send(str(author) + " 2회를 받으셨습니다. 서버에서 추방됬니다.")
+                    await message.channel.send(str(author) + "님은 경고 2회누적으로 서버에서 추방되었습니다.")
                 else:
-                    await message.channel.send(str(author) + "경고를 1회 받았습니다")
+                    await message.channel.send(str(author) + "님은 경고를 1회 받았습니다")
                     sheet["c" + str(i)].value = why
                 break
             if sheet["A" + str(i)].value == None:
@@ -53,10 +51,9 @@ async def on_message(message):
                 sheet["B" + str(i)].value = 1
                 sheet["c" + str(i)].value = why
                 file.save("경고.xlsx")
-                await message.channel.send(str(author) + "경고를 1회 받았습니다.")
+                await message.channel.send(str(author) + "님은 경고를 1회 받았습니다.")
                 break
-            i += 1    
-
+            i += 1
             
 
 
